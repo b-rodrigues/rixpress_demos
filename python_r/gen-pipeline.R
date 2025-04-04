5f2a3164a34b009f6fc200bb8dcabc494f9537a7
Bruno Rodrigues <bruno@brodrigues.co>
2025-04-04 10:56:31 +0200

    passing data from python to R

--- python_r/gen-pipeline.R
+++ python_r/gen-pipeline.R
@@ -4,20 +4,19 @@ library(igraph)
 d0 <- rxp_py_file(
   name = mtcars_pl,
   path = 'data/mtcars.csv',
-  read_function = "lambda x: polars.read_csv(x, separator='|')",
+  read_function = "lambda x: polars.read_csv(x, separator='|')"
 )
 
 d1 <- rxp_py(
   # reticulate doesn't support polars DFs yet, so need to convert
   # first to pandas DF
   name = mtcars_pl_am,
-  py_expr = "mtcars_pl.filter(polars.col('am') == 1).to_pandas()",
+  py_expr = "mtcars_pl.filter(polars.col('am') == 1).to_pandas()"
 )
 
-# Convert pandas DF to R DF
 d2 <- rxp_py2r(
   name = mtcars_am,
-  expr = mtcars_pl_am,
+  expr = mtcars_pl_am
 )
 
 d3 <- rxp_r(
@@ -26,9 +25,19 @@ d3 <- rxp_r(
   additional_files = "functions.R"
 )
 
-d4 <- rxp_r(
+d3_1 <- rxp_r2py(
+  name = mtcars_head_py,
+  expr = mtcars_head
+)
+
+d4 <- rxp_py(
+  name = mtcars_tail_py,
+  py_expr = 'mtcars_head_py.tail()'
+)
+
+d4_1 <- rxp_py2r(
   name = mtcars_tail,
-  expr = tail(mtcars_head)
+  expr = mtcars_tail_py
 )
 
 d5 <- rxp_r(
@@ -42,7 +51,7 @@ doc <- rxp_quarto(
   additional_files = c("content.qmd", "images")
 )
 
-rxp_list <- list(d0, d1, d2, d3, d4, d5, doc)
+rxp_list <- list(d0, d1, d2, d3, d3_1, d4, d4_1, d5, doc)
 
 rixpress(rxp_list, project_path = ".")
 
