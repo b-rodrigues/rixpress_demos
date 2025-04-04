fc215079b98cba6cc3e4e5097d2ba3446f6f446c
Bruno Rodrigues <bruno@brodrigues.co>
2025-04-04 10:44:46 +0200

    view dag in CI

--- python_r/gen-pipeline.R
+++ python_r/gen-pipeline.R
@@ -45,4 +45,12 @@ rxp_list <- list(d0, d1, d2, d3, d4, d5, doc)
 
 rixpress(rxp_list, project_path = ".")
 
-plot_dag()
+# Plot DAG for CI
+dag_obj <- plot_dag(return_igraph = TRUE)
+
+dag_obj <- set_vertex_attr(dag_obj, "label", value = V(dag_obj)$name)
+
+# Step 2: Delete the "name" attribute
+dag_obj <- delete_vertex_attr(dag_obj, "name")
+
+igraph::write_graph(dag_obj, file = "dag.dot", format = "dot")
