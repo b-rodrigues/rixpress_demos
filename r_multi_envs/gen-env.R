a2d311d65d35e93adc00cc5d8b94c769e11ede10
Bruno Rodrigues <bruno@brodrigues.co>
2025-04-04 17:13:12 +0200

    r_multi_envs pipeline

---
+++ r_multi_envs/gen-env.R
@@ -0,0 +1,56 @@
+library(rix)
+
+# Environment to run some derivations
+rix(
+  date = "2025-03-31",
+  r_pkgs = c("dplyr"),
+  ide = "none",
+  project_path = ".",
+  overwrite = TRUE
+)
+
+file.rename("default.nix", "default2.nix")
+
+# Environment to compile Quarto
+rix(
+  date = "2025-03-31",
+  r_pkgs = c("quarto", "chronicler"),
+  git_pkgs = list(
+    list(
+      package_name = "rix",
+      repo_url = "https://github.com/ropensci/rix/",
+      commit = "HEAD"
+    ),
+    list(
+      package_name = "rixpress",
+      repo_url = "https://github.com/b-rodrigues/rixpress",
+      commit = "HEAD"
+    )
+  ),
+  ide = "none",
+  project_path = ".",
+  overwrite = TRUE
+)
+
+file.rename("default.nix", "quarto-env.nix")
+
+# Main environment to run the pipeline
+rix(
+  date = "2025-03-31",
+  r_pkgs = c("igraph"),
+  git_pkgs = list(
+    list(
+      package_name = "rix",
+      repo_url = "https://github.com/ropensci/rix/",
+      commit = "HEAD"
+    ),
+    list(
+      package_name = "rixpress",
+      repo_url = "https://github.com/b-rodrigues/rixpress",
+      commit = "HEAD"
+    )
+  ),
+  ide = "none",
+  project_path = ".",
+  overwrite = TRUE
+)
---
+++ r_multi_envs/gen-pipeline.R
@@ -0,0 +1,48 @@
+library(rixpress)
+library(igraph)
+
+d0 <- rxp_r_file(
+  mtcars,
+  'mtcars.csv',
+  \(x) (read.csv(file = x, sep = "|")),
+  nix_env = "default.nix"
+)
+d1 <- rxp_r(mtcars_am, filter(mtcars, am == 1), nix_env = "default2.nix")
+
+d2 <- rxp_r(
+  mtcars_head,
+  my_head(mtcars_am),
+  additional_files = "functions.R",
+  nix_env = "default.nix"
+)
+
+d3 <- rxp_r(
+  mtcars_tail,
+  my_tail(mtcars_head),
+  additional_files = "functions.R",
+  nix_env = "default.nix"
+)
+
+d4 <- rxp_r(mtcars_mpg, select(mtcars_tail, mpg), nix_env = "default2.nix")
+
+doc <- rxp_quarto(
+  page,
+  "page.qmd",
+  additional_files = c("content.qmd", "images"),
+  nix_env = "quarto-env.nix"
+)
+
+derivs <- list(d0, d1, d2, d3, d4, doc)
+
+rixpress(derivs, project_path = ".")
+
+
+# Plot DAG for CI
+dag_obj <- plot_dag(return_igraph = TRUE)
+
+dag_obj <- set_vertex_attr(dag_obj, "label", value = V(dag_obj)$name)
+
+# Step 2: Delete the "name" attribute
+dag_obj <- delete_vertex_attr(dag_obj, "name")
+
+igraph::write_graph(dag_obj, file = "dag.dot", format = "dot")
