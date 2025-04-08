3d4aff93877827c3f13eec0d41a1fadb486eda4a
Bruno Rodrigues <bruno@brodrigues.co>
2025-04-08 16:57:03 +0200

    Update gen-pipeline.R

--- yanai_lercher_2020/gen-pipeline.R
+++ yanai_lercher_2020/gen-pipeline.R
@@ -55,7 +55,8 @@ doc <- rxp_quarto(
 
 rxp_list <- list(d0, d1, d2, d3, d4, d5, d6, d7, doc)
 
-rixpress(rxp_list, project_path = ".")
+# Set build to FALSE, because I need to adjust imports
+rixpress(rxp_list, project_path = ".", build = FALSE)
 
 adjust_imports("import pillow", "from PIL import Image")
 
