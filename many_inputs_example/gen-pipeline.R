9b8d6592917ce7a6de953aafb93be5d01e737611
Bruno Rodrigues <bruno@brodrigues.co>
2025-04-10 15:38:56 +0200

    build many inputs pipelines

--- many_inputs_example/gen-pipeline.R
+++ many_inputs_example/gen-pipeline.R
@@ -1,8 +1,8 @@
 library(rixpress)
 
 list(
-  hu = rxp_r_file(
-    name = mtcars_pl,
+  rxp_r_file(
+    name = mtcars_parts,
     path = 'data',
     read_function = \(x)(readr::read_delim(list.files(x, full.names = TRUE), delim = '|')),
     copy_data_folder = TRUE
