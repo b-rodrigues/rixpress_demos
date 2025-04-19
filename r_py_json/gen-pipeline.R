f8ee5df6cbfbc7b4cb0ec4b06f56fb12ed63b6e0
Bruno Rodrigues <bruno@brodrigues.co>
2025-04-20 00:07:10 +0200

    remove unneededn quotes

--- r_py_json/gen-pipeline.R
+++ r_py_json/gen-pipeline.R
@@ -3,27 +3,27 @@ library(igraph)
 
 list(
   rxp_py_file(
-    name = "mtcars_pl",
+    name = mtcars_pl,
     path = "data/mtcars.csv",
     read_function = "lambda x: polars.read_csv(x, separator='|')"
   ),
 
   rxp_py(
-    name = "mtcars_pl_am",
+    name = mtcars_pl_am,
     py_expr = "mtcars_pl.filter(polars.col('am') == 1)",
     additional_files = "functions.py",
     serialize_function = "serialize_to_json",
   ),
 
   rxp_r(
-    name = "mtcars_head",
+    name = mtcars_head,
     expr = my_head(mtcars_pl_am),
     additional_files = "functions.R",
     unserialize_function = "jsonlite::fromJSON"
   ),
 
   rxp_r(
-    name = "mtcars_mpg",
+    name = mtcars_mpg,
     expr = dplyr::select(mtcars_head, mpg)
   )
 ) |>
