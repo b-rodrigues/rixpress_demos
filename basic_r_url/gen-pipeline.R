c3ce30da8252ae090b3c818e9ca6e44f151838df
Bruno Rodrigues <bruno@brodrigues.co>
2025-05-03 21:38:26 +0200

    basic_r_url demo

---
+++ basic_r_url/gen-pipeline.R
@@ -0,0 +1,24 @@
+library(rixpress)
+library(igraph)
+
+list(
+  rxp_r_file(
+    name = mtcars,
+    path = 'https://raw.githubusercontent.com/b-rodrigues/rixpress_demos/refs/heads/master/basic_r/data/mtcars.csv',
+    read_function = \(x) (read.csv(file = x, sep = "|"))
+  ),
+
+  rxp_r(
+    name = filtered_mtcars,
+    expr = dplyr::filter(mtcars, am == 1)
+  ),
+
+  rxp_r(
+    name = mtcars_mpg,
+    expr = dplyr::select(filtered_mtcars, mpg)
+  )
+) |>
+  rixpress(project_path = ".", build = FALSE)
+
+# Plot DAG for CI
+dag_for_ci()
---
+++ basic_r_url/readme.md
@@ -0,0 +1,4 @@
+# Very simple pipeline to gently introduce rixpress
+
+Code from the "Basic usage" vignette of the rixpress package,
+but the path to the dataset is an url.
