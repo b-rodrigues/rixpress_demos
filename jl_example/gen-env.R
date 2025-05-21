a1adce20d5623a70a574b8640294f68e3e139140
Bruno Rodrigues <bruno@brodrigues.co>
2025-05-21 15:10:10 +0200

    added jl example

---
+++ jl_example/gen-env.R
@@ -0,0 +1,36 @@
+library(rix)
+
+rix(
+  date = "2025-05-21",
+  r_pkgs = c(
+    "arrow",
+    "dplyr",
+    "tidyr",
+    "ggplot2",
+    "hexbin",
+  ),
+  git_pkgs = list(
+    list(
+      package_name = "rix",
+      repo_url = "https://github.com/ropensci/rix/",
+      commit = "HEAD"
+    ),
+    list(
+      package_name = "rixpress",
+      repo_url = "https://github.com/b-rodrigues/rixpress",
+      commit = "HEAD"
+    )
+  ),
+  jl_conf = list(
+    jl_version = "1.10",
+    jl_pkgs = c(
+      "Arrow",
+      "DataFrames",
+      "SparseArrays",
+      "LinearAlgebra"
+    )
+  ),
+  ide = "none",
+  project_path = ".",
+  overwrite = TRUE
+)
