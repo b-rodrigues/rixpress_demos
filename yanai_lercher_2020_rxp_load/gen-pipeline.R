840637a27741c488cecbb46302a1a9bf6fa5a78a
Bruno Rodrigues <bruno@brodrigues.co>
2025-07-10 16:16:14 +0200

    added rxp_load in quarto doc

---
+++ yanai_lercher_2020_rxp_load/gen-pipeline.R
@@ -0,0 +1,64 @@
+library(rixpress)
+library(igraph)
+
+d0 <- rxp_py_file(
+  name = gorilla_pixels,
+  path = 'md_source/gorilla/gorilla-waving-cartoon-black-white-outline-clipart-914.jpg',
+  read_function = "read_image"
+)
+
+d1 <- rxp_py(
+  name = threshold_level,
+  py_expr = "50"
+)
+
+d2 <- rxp_py(
+  name = py_coords,
+  py_expr = "numpy.column_stack(numpy.where(gorilla_pixels < threshold_level))",
+  additional_files = "functions.py"
+)
+
+d3 <- rxp_py2r(
+  name = raw_coords,
+  expr = py_coords
+)
+
+d4 <- rxp_r(
+  name = coords,
+  expr = clean_coords(raw_coords),
+  additional_files = "functions.R"
+)
+
+d5 <- rxp_r(
+  name = gender_dist,
+  expr = gender_distribution(coords),
+  additional_files = "functions.R"
+)
+
+d6 <- rxp_r(
+  name = plot1,
+  expr = make_plot1(coords),
+  additional_files = "functions.R"
+)
+
+d7 <- rxp_r(
+  name = plot2,
+  expr = make_plot2(coords),
+  additional_files = "functions.R"
+)
+
+doc <- rxp_qmd(
+  name = doc,
+  qmd_file = "md_source/source.qmd",
+  additional_files = c("md_source/gorilla")
+)
+
+rxp_list <- list(d0, d1, d2, d3, d4, d5, d6, d7, doc)
+
+# Set build to FALSE, because I need to adjust imports
+rixpress(rxp_list, project_path = ".", build = FALSE)
+
+adjust_import("import pillow", "from PIL import Image")
+
+# Plot DAG for CI
+dag_for_ci()
---
+++ yanai_lercher_2020_rxp_load/readme.md
@@ -0,0 +1,15 @@
+## rixpress port of 'Selective attention in hypothesis-driven data analysis'
+
+This pipeline is similar to the example found in `python_r` in that it mixes
+both R and Python code and ultimately outputs an HTML document.
+
+You can see the final HTML document
+[here](https://b-rodrigues.github.io/rixpress_demos/yanai_lercher/index.html).
+
+This is inspired by the code found in [this blog
+post](https://ivelasq.rbind.io/blog/reticulate-data-recreation/). Thank you to
+[Isabella Velásquez](https://fosstodon.org/deck/@ivelasq3/114293176424941566)
+for the suggestion!
+
+You can find the original paper [here](https://www.biorxiv.org/content/10.1101/2020.07.30.228916v1).
+
