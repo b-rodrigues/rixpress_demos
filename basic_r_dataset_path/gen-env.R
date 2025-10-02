a69866c8099ab68a3d04048e8ab827d33eb4d243
Bruno Rodrigues <bruno@brodrigues.co>
2025-10-02 22:22:54 +0200

    new example with path to data dataset/

---
+++ basic_r_dataset_path/gen-env.R
@@ -0,0 +1,19 @@
+library(rix)
+
+# Define execution environment
+rix(
+  date = "2025-09-11",
+  r_pkgs = c("dplyr", "igraph"),
+  py_conf = list(
+    py_version = "3.13",
+    py_pkgs = c("ryxpress", "biocframe", "rds2py", "pandas")
+  ),
+  git_pkgs = list(
+    package_name = "rixpress",
+    repo_url = "https://github.com/ropensci/rixpress",
+    commit = "HEAD"
+  ),
+  ide = "none",
+  project_path = ".",
+  overwrite = TRUE
+)
---
+++ basic_r_dataset_path/gen-pipeline.R
@@ -0,0 +1,24 @@
+library(rixpress)
+library(igraph)
+
+list(
+  rxp_r_file(
+    name = mtcars,
+    path = 'dataset/mtcars.csv',
+    read_function = \(x) (read.csv(file = x, sep = "|"))
+  ),
+
+  rxp_r(
+    name = filtered_mtcars,
+    expr = dplyr::filter(mtcars, am == 1)
+  ),
+
+  rxp_r(
+    name = mtcars_mpg,
+    expr = dplyr::select(filtered_mtcars, mpg)
+  )
+) |>
+  rxp_populate(project_path = ".", build = FALSE)
+
+# Plot DAG for CI
+rxp_dag_for_ci()
